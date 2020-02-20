ebbe23026c463c99b989136ce7c7b3b01d03e841
ivy-1996 <919624032@qq.com>
2020-02-20 10:19:28 +0800

    update a version

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from distutils.core import setup
 
 setup(name='djangowechatapi',
-      version='0.4',
+      version='0.5',
       description='wechat api for web',
       author='ivy',
       author_email='919624032@qq.com',
-      url='https://github.com/Ivy-1996/wechatapi.git',
+      url='https://github.com/Ivy-1996/wechatapi',
       packages=['wechat', 'wechat.core', 'wechat.management', 'wechat.management.commands', 'wechat.migrations'],
       python_requires='>=3.6',
+      license='Apache License',
       install_requires=[
           'djangorestframework==3.11.0',
           'wxpy',
           'django_redis',
           'requests',
           'django_filter',
-          'django=2.2.0',
+          'django=2.1.5',
           'pillow',
       ]
       )
